8ea9b7181a3ab00cb561e2cd642e9d7689f37196
PraktikumDaspro <email@ristapolinema03.com>
2025-09-25 11:11:41 +0700

    Menambahkan user 2

--- Text.java
+++ Text.java
@@ -1,8 +1,10 @@
 public class Text {
     public static void main(String[] args){
         System.out.println("Hi, ini baris kode pertama");
-        System.out.println("User 1 menambhakan baris baru");
-    
+        System.out.println("User 1 menambahakan baris baru");
+        System.out.println("Hi, ini baris kode pertama");
+        System.out.println("User 2 menambahakan baris baru");
+        
     }
 
 }
